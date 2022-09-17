a9d02fe3c116f4d068fde8b23ed6f2f73696c03e
Sharon Frenkel <sharonfrenkel@Sharons-MacBook-Pro.local>
2022-09-17 11:37:06 +0300

    initialize repository

---
+++ js/app.js
@@ -0,0 +1,118 @@
+'use strict'
+
+const WALL = 'WALL'
+const FLOOR = 'FLOOR'
+const BALL = 'BALL'
+const GAMER = 'GAMER'
+
+const GAMER_IMG = '<img src="img/gamer.png">'
+const BALL_IMG = '<img src="img/ball.png">'
+
+// Model:
+var gBoard
+var gGamerPos
+
+function initGame() {
+	gGamerPos = { i: 2, j: 9 }
+	gBoard = buildBoard()
+	renderBoard(gBoard)
+
+}
+
+function buildBoard() {
+	const board = []
+	// TODO: Create the Matrix 10 * 12 
+	// TODO: Put FLOOR everywhere and WALL at edges
+	// TODO: Place the gamer and two balls
+	console.log(board)
+	return board
+}
+
+// Render the board to an HTML table
+function renderBoard(board) {
+
+	const elBoard = document.querySelector('.board')
+	var strHTML = ''
+	for (var i = 0; i < board.length; i++) {
+		strHTML += '<tr>\n'
+		for (var j = 0; j < board[0].length; j++) {
+			const currCell = board[i][j]
+
+			var cellClass = getClassName({ i: i, j: j })
+
+			if (currCell.type === FLOOR) cellClass += ' floor'
+			else if (currCell.type === WALL) cellClass += ' wall'
+
+			strHTML += '\t<td class="cell ' + cellClass + '"  onclick="moveTo(' + i + ',' + j + ')" >\n'
+
+			if (currCell.gameElement === GAMER) {
+				strHTML += GAMER_IMG
+			} else if (currCell.gameElement === BALL) {
+				strHTML += BALL_IMG
+			}
+
+			strHTML += '\t</td>\n'
+		}
+		strHTML += '</tr>\n'
+	}
+	console.log('strHTML is:')
+	console.log(strHTML)
+	elBoard.innerHTML = strHTML
+}
+
+// Move the player to a specific location
+function moveTo(i, j) {
+
+	const targetCell = gBoard[i][j]
+	if (targetCell.type === WALL) return
+
+	// Calculate distance to make sure we are moving to a neighbor cell
+	const iAbsDiff = Math.abs(i - gGamerPos.i)
+	const jAbsDiff = Math.abs(j - gGamerPos.j)
+
+	// If the clicked Cell is one of the four allowed
+	if ((iAbsDiff === 1 && jAbsDiff === 0) || (jAbsDiff === 1 && iAbsDiff === 0)) {
+
+		if (targetCell.gameElement === BALL) {
+			console.log('Collecting!')
+		}
+
+		// TODO: Move the gamer
+
+	} else console.log('TOO FAR', iAbsDiff, jAbsDiff)
+
+}
+
+// Convert a location object {i, j} to a selector and render a value in that element
+function renderCell(location, value) {
+	const cellSelector = '.' + getClassName(location)
+	const elCell = document.querySelector(cellSelector)
+	elCell.innerHTML = value
+}
+
+// Move the player by keyboard arrows
+function handleKey(event) {
+	const i = gGamerPos.i
+	const j = gGamerPos.j
+
+	switch (event.key) {
+		case 'ArrowLeft':
+			moveTo(i, j - 1)
+			break
+		case 'ArrowRight':
+			moveTo(i, j + 1)
+			break
+		case 'ArrowUp':
+			moveTo(i - 1, j)
+			break
+		case 'ArrowDown':
+			moveTo(i + 1, j)
+			break
+	}
+}
+
+// Returns the class name for a specific cell
+function getClassName(location) {
+	const cellClass = 'cell-' + location.i + '-' + location.j
+	return cellClass
+}
